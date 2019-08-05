6532305d399c9ed81f6e087510762a49ee8c76bc
Sean Mongru <31765998+SeanMongru@users.noreply.github.com>
2019-08-05 19:09:36 +0100

    Inital commit after takeover

---
+++ src/org/oop_practice_1/version1/initial_try.java
@@ -0,0 +1,10 @@
+package org.oop_practice_1.version1;
+
+public class initial_try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test number 4");
+	}
+
+}
